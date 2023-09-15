4917cf282f27d3d0abfeed64ce5e4ef058b39db4
CHEETAH02 <chetansathish02@gmail.com>
2023-09-16 01:16:48 +0530

    New model with XGB

--- Backend/model.py
+++ Backend/model.py
@@ -16,15 +16,14 @@ from lime.lime_tabular import LimeTabularExplainer
 df = pd.read_csv("Backend/final.csv")
 df = df.dropna()
 
-df['url'] = df['Protocol']+"://"+df['Domain']+df['Path']
+df["url"] = df["Protocol"] + "://" + df["Domain"] + df["Path"]
 
-df['age_domain'].value_counts()
-df['statistical_report'].value_counts()
-df['dns_record'].value_counts()
+df["age_domain"].value_counts()
+df["statistical_report"].value_counts()
+df["dns_record"].value_counts()
 
 
 def slashes(url):
-
     # matched = re.search(dot_pattern, url)
     if len(url.split("/")) > 5:
         return 1
@@ -32,8 +31,8 @@ def slashes(url):
         return 0
 
 
-df['slashes'] = df['url'].apply(lambda i: slashes(i))
-df['slashes'].value_counts()
+df["slashes"] = df["url"].apply(lambda i: slashes(i))
+df["slashes"].value_counts()
 
 
 def dots(url):
@@ -45,19 +44,33 @@ def dots(url):
         return 0
 
 
-df['dots'] = df['url'].apply(lambda i: dots(i))
-df['dots'].value_counts()
+df["dots"] = df["url"].apply(lambda i: dots(i))
+df["dots"].value_counts()
 
-y = df['label']
+y = df["label"]
 # x = df.drop('Label', axis=1)
-x = df.drop(['Domain', 'Path', 'Protocol', 'label', 'Having_@_symbol',
-            'Redirection_//_symbol', 'Having_IP', 'web_traffic', 'url', 'http_tokens'], axis=1)
+x = df.drop(
+    [
+        "Domain",
+        "Path",
+        "Protocol",
+        "label",
+        "Having_@_symbol",
+        "Redirection_//_symbol",
+        "Having_IP",
+        "web_traffic",
+        "url",
+        "http_tokens",
+    ],
+    axis=1,
+)
 print(x.shape, y.shape)
 
 print(x.columns)
 
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2, random_state=42, shuffle=True)
+    x, y, test_size=0.2, random_state=42, shuffle=True
+)
 x_train.shape, y_train.shape, x_test.shape, y_test.shape
 
 
@@ -88,11 +101,26 @@ y_train_forest = forest.predict(x_train)
 acc_train_forest = accuracy_score(y_train, y_train_forest)
 acc_test_forest = accuracy_score(y_test, y_test_forest)
 
-print("Random forest: Accuracy on training Data: {:.3f}".format(
-    acc_train_forest))
+print("Random forest: Accuracy on training Data: {:.3f}".format(acc_train_forest))
 print("Random forest: Accuracy on test Data: {:.3f}".format(acc_test_forest))
 print("Classification report - \n", classification_report(y_test, y_test_forest))
 print(confusion_matrix(y_test, y_test_forest))
+
+import xgboost as xgb
+
+xgb_model = xgb.XGBClassifier(learning_rate=0.3, n_estimators=100, max_depth=6)
+xgb_model.fit(x_train, y_train)
+y_test_xgb = xgb_model.predict(x_test)
+y_train_xgb = xgb_model.predict(x_train)
+
+# computing the accuracy of the model performance
+acc_train_xgb = accuracy_score(y_train, y_train_xgb)
+acc_test_xgb = accuracy_score(y_test, y_test_xgb)
+
+print("XGB: Accuracy on training Data: {:.3f}".format(acc_train_xgb))
+print("XGB: Accuracy on test Data: {:.3f}".format(acc_test_xgb))
+print("Classification report - \n", classification_report(y_test, y_test_xgb))
+print(confusion_matrix(y_test, y_test_xgb))
 
 # class_names = ['original', 'phishing']
 # feature_names = list(x_train.columns)
@@ -103,4 +131,5 @@ print(confusion_matrix(y_test, y_test_forest))
 # explanation = explainer.explain_instance(instance, forest.predict_proba)
 # explanation.show_in_notebook()
 
-joblib.dump(forest, 'Backend/model.sav')
+
+joblib.dump(xgb_model, "model.sav")
