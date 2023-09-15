3f4c539ae0048c6e867ce3a239a76c27ac10f834
CHEETAH02 <chetansathish02@gmail.com>
2023-09-16 01:34:11 +0530

    Updated App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
 			<BrowserRouter>
 				<DrawerAppBar />
 				<Routes>
+					<Route path="/" element={<Home />} />
 					<Route path="/home" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/scenarios" element={<ControlledAccordions />} />
